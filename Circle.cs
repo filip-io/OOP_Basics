12ec4842f175f2881c366067bdd40ba21c04c28c
Filip Nilsson <filip.n@outlook.com>
2023-10-21 19:47:25 +0200

    Add project files.

---
+++ Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Basics
+{
+    class Circle
+    {
+        private double _radius { get; set; }
+
+
+        public Circle(double radius)
+        {
+            _radius = radius;
+        }
+
+
+        public double GetArea()
+        {
+            return _radius * _radius * Math.PI;
+        }                    
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+// Filip Nilsson NET23
+
+namespace OOP_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle FirstCircle = new Circle(5);
+            double area = FirstCircle.GetArea();
+            Console.WriteLine($"The area of your first circle is: {area:#.##}");
+
+            Circle SecondCircle = new Circle(6);
+            double area2 = SecondCircle.GetArea();
+            Console.WriteLine($"The area of your second circle is: {area2:#.##}");
+
+            Console.ReadLine();
+        }
+    }
+}
